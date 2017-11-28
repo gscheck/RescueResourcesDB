29be576518346b522eda8df0bdf9bf9906d789fa
Garth Scheck <garth.scheck@bellevuecollege.edu>
2017-11-27 17:35:14 -0800

    updated about page

--- htdocs/About.php
+++ htdocs/About.php
@@ -19,13 +19,21 @@ include_once('navbar.php');
          <div class="container-fluid">
 		 <h2 style="margin-left: 140px; margin-top: 50px; letter-spacing: 1px; font-family: 'Dancing Script', cursive; font-size: 36pt; font-weight: bolder;">About</h2><br />
 		 
-		<p style="margin-left: 130px; font-family: 'Montserrat', sans-serif; font-weight: bolder; color: gray;">&nbsp;&nbsp;This application intended to keep track of resources required for animal rescue.</p>
-			
+		<p style="margin-left: 130px; font-family: 'Montserrat', sans-serif; font-weight: bolder; color: gray;">&nbsp;&nbsp;“When you’ve looked in the eyes of a dumped/left behind animal and wanted to erase all their hurt and pain, <br />
+		&nbsp;&nbsp;while at the same time wanting to hurt the human responsible for causing it, <br />
+		&nbsp;&nbsp;you might be an Animal Rescuer!!”<br />
+		&nbsp;&nbsp;-	Anonymous</p> 
+		
+		<p style="margin-left: 130px; font-family: 'Montserrat', sans-serif; font-weight: bolder; color: gray;">&nbsp;&nbsp;A special thanks to all the volunteers and other animal rescuers, for not only caring, but actually doing something about it too. <br /> 
+		A little background about Key to Happiness Rescue; it was formed in Texas in the beginning of 2016, and has branched out to Louisiana and Wisconsin. <br />
+		It was established to help find permanent, loving and secure homes for rescued animals. For more information visit WWW.KTHR.ORG.</p>
+		
+		
+		<p style="margin-left: 130px; font-family: 'Montserrat', sans-serif; font-weight: bolder; color: gray;">&nbsp;&nbsp;This database was created to efficiently record and retrieve information on who is able to provide help and supplies, 
+		as well as track down the kind of help they are providing, thus preventing delays which will lead to many more animals being rescued over time. This database can be used to ease many of the daily routine tasks, by having quick access to records of all volunteers, supplies, and activities to be done.</p> 
+		<br /><br />
 		<div style="margin-left: 130px; font-family: 'Montserrat', sans-serif;"> <p style="color: gray;">&nbsp;&nbsp;<b>Developed by:</b></p>
-			  <p style="margin-top: 10px; margin-left: -10px; font-weight: bolder;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Bridget Wellen </p>
-			  <p style="margin-left: -10px;  font-weight: bolder;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Aniss Beyzaee</p>
-			  <p style="margin-left: -10px; font-weight: bolder;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Vida Movahedi</p>
-			  <p style="margin-left: -10px;  font-weight: bolder;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Garth Scheck </P>
+			  <p style="margin-top: 10px; margin-left: -10px; font-weight: bolder;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Bridget Wellen, Aniss Beyzaee, Vida Movahedi, Garth Scheck</p>
 		</div>
         
       </form>
